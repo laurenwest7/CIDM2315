ec4cebc11244e13113e8746bd7aa393c432ce773
laurenwest7 <123128812+laurenwest7@users.noreply.github.com>
2023-02-09 20:33:52 -0600

    Add files via upload

---
+++ Homework4_Q1andQ2.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+bool flag = true;
+    Console.WriteLine("Input an integer: ");
+    string input = Console.ReadLine();
+    int number = Convert.ToInt32(input);
+    for(int i=2;i<number/2;i++)
+    {
+        if(number%i==0)
+        {
+               flag = false;
+               break;
+        }
+    }
+    if(flag)
+    {
+        Console.WriteLine( input +" is Prime ");
+    }
+    else{
+        Console.WriteLine(input+" is non-prime ");
+    }
+    }    
+}
+
+
+namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Assign an int value to N: ");
+        int n = int.Parse(Console.ReadLine());
+        
+        for(int i = 0;i<n;i++){
+            
+        for(int j =0;j<n;j++){
+            Console.Write("#");
+        }
+            Console.WriteLine();
+        }
+  }
+
+    }
+
+
+
