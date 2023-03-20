590ce6d4cfec1a128f90fed58b1004ed3fd5f5f6
laurenwest7 <123128812+laurenwest7@users.noreply.github.com>
2023-03-20 18:15:31 -0500

    Add files via upload

---
+++ hw7.cs
@@ -0,0 +1,47 @@
+﻿namespace Hw7;
+class Program
+{
+    class Customer
+{
+
+    private int cus_id;
+    public string cus_name;
+    public int cus_age;
+    public Customer(int id, string name, int age)
+
+    {
+        cus_id = id;
+        cus_name = name;
+        cus_age = age;
+    }
+
+
+    public void ChangeID(int new_id)
+
+    {
+        cus_id = new_id;
+    }
+
+    public void PrintCusInfo()
+    {
+        Console.WriteLine("Customer:" + cus_id +", name: " + cus_name + ", age: " + cus_age);
+    }
+}
+
+class Homework7
+{
+
+    static void Main()
+    {
+        Customer cust1 = new Customer(110, "Alice", 28);
+        cust1.PrintCusInfo();
+        Customer cust2 = new Customer(111, "Bob", 30);
+        cust2.PrintCusInfo();
+        cust1.ChangeID(220);
+        cust2.ChangeID(221);
+        cust1.PrintCusInfo();
+        cust2.PrintCusInfo();
+    }
+
+}
+}
